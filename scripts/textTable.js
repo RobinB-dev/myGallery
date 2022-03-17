7f90a8148c2353c98afb3bda66bef9c0b4529ed9
Rob1 <robin.bourachot@hetic.net>
2022-03-17 17:15:38 +0100

    Update textTable.js

--- scripts/textTable.js
+++ scripts/textTable.js
@@ -1,58 +1,66 @@
-let textTable = [    
+let textTable = [
   {
     imgName: "Mucha.png",
     projectName: "Projet 1",
-    subtitle: "Affiche de film",
+    subtitle: "Architecture 1",
     date: "02 2021",
-    explications: "Cette affiche a été réalisée dans le cadre d'un travail pour M Lignel (culture de l'image).<br>Le sujet était la réalisation de l'affiche du film de notre choix à la manière d'Alfons Mucha, artiste tchécoslovaque, fer-de-lance du style « Art nouveau ».<br> J'ai choisi l'excellent film « American Beauty » dont on retrouve les éléments principaux dans un style et une mise en page empruntés à Mucha.",
+    explications:
+      "Cette affiche a été réalisée dans le cadre d'un travail pour M Lignel (culture de l'image).<br>Le sujet était la réalisation de l'affiche du film de notre choix à la manière d'Alfons Mucha, artiste tchécoslovaque, fer-de-lance du style « Art nouveau ».<br> J'ai choisi l'excellent film « American Beauty » dont on retrouve les éléments principaux dans un style et une mise en page empruntés à Mucha.",
   },
   {
     imgName: "Bauhaus.png",
     projectName: "Projet 2",
-    subtitle: "Affiche pour une exposition",
+    subtitle: "Architecture 2",
     date: "01 2021",
-    explications: "Cette affiche a été réalisée dans le cadre d'un travail pour M Lignel (culture de l'image).<br>Le but de ce travail était la réalisation d'une affiche pour le Musée des Arts Décoratifs qui met en place une exposition intitilée : le fonctionnalisme, Gropius-Rams-Ive.<br>Cette exposition retrace l'histoire du fonctionnalisme depuis le Bauhaus jusqu’à Apple.<br>Sur cette affiche les objets phares du mouvement Bauhaus sont représentés à la façon de schémas, mettant en avant les fonctions premières de l'objet. On retrouvre aussi le logo du Bauhaus au coeur de la pomme d'Apple",
-  },  
+    explications:
+      "Cette affiche a été réalisée dans le cadre d'un travail pour M Lignel (culture de l'image).<br>Le but de ce travail était la réalisation d'une affiche pour le Musée des Arts Décoratifs qui met en place une exposition intitilée : le fonctionnalisme, Gropius-Rams-Ive.<br>Cette exposition retrace l'histoire du fonctionnalisme depuis le Bauhaus jusqu’à Apple.<br>Sur cette affiche les objets phares du mouvement Bauhaus sont représentés à la façon de schémas, mettant en avant les fonctions premières de l'objet. On retrouvre aussi le logo du Bauhaus au coeur de la pomme d'Apple",
+  },
   {
     imgName: "Constructivisme.png",
     projectName: "Projet 3",
     subtitle: "Affiche de film",
     date: "11 2020",
-    explications: "Cette affiche a été réalisée dans le cadre d'un travail pour M Lignel (culture de l'image).<br>Ce travail consiste à la réalisation d'une affiche pour le film « Demain », un documentaire sur la crise écologique et économique post 2008, à l'occasion de sa sortie sur Netflix.<br>Cette affiche doit reprendre le style graphique du mouvement constructiviste, qui est un courant qui puise sa source dans la révolution russe et par conséquent qui met en avant l'industrie qui était en plein essor.<br>C'est le juste équilibre entre ses deux notions plutôt opposées que j'ai essayé de trouver. En mettant en avant, de façon quasi propagandiste, les solutions présentées dans le film pour répondre aux problèmes actuels.",
+    explications:
+      "Cette affiche a été réalisée dans le cadre d'un travail pour M Lignel (culture de l'image).<br>Ce travail consiste à la réalisation d'une affiche pour le film « Demain », un documentaire sur la crise écologique et économique post 2008, à l'occasion de sa sortie sur Netflix.<br>Cette affiche doit reprendre le style graphique du mouvement constructiviste, qui est un courant qui puise sa source dans la révolution russe et par conséquent qui met en avant l'industrie qui était en plein essor.<br>C'est le juste équilibre entre ses deux notions plutôt opposées que j'ai essayé de trouver. En mettant en avant, de façon quasi propagandiste, les solutions présentées dans le film pour répondre aux problèmes actuels.",
   },
   {
-    imgName: "Heticien.jpg",  
+    imgName: "Heticien.jpg",
     projectName: "Projet 4",
     subtitle: "Affiche d'un journal étudiant",
     date: "09 2020",
-    explications: "Cette affiche a été réalisée dans le cadre d'un travail pour M de Torquat (information en question).<br> Le travail portait sur les différentes visions que les élèves pouvaient porter sur des problématiques liées a la crise du Covid. <br> Nous étions totalement libres quant au format du rendu. <br> J'ai choisi de le mettre sous la forme d'un journal, car l’enchaînement questions-réponses me faisait penser à une interview. <br> Comme ce journal était écrit par des élèves pour des élèves, j'ai imaginé que ce serait judicieux de l'afficher au sein d'Hetic, c'est pour cela que j'ai ajouté l'effet d'affiche collée au mur.",
-  },  
+    explications:
+      "Cette affiche a été réalisée dans le cadre d'un travail pour M de Torquat (information en question).<br> Le travail portait sur les différentes visions que les élèves pouvaient porter sur des problématiques liées a la crise du Covid. <br> Nous étions totalement libres quant au format du rendu. <br> J'ai choisi de le mettre sous la forme d'un journal, car l’enchaînement questions-réponses me faisait penser à une interview. <br> Comme ce journal était écrit par des élèves pour des élèves, j'ai imaginé que ce serait judicieux de l'afficher au sein d'Hetic, c'est pour cela que j'ai ajouté l'effet d'affiche collée au mur.",
+  },
   {
     imgName: "Savoir.jpg",
     projectName: "Projet 5",
     subtitle: "Affiche pour le lancement d'un journal",
     date: "05 2020",
-    explications: "Cette affiche a été réalisée dans le cadre d'un travail pour Mme Boissier (culture générale).<br> Le travail consistait en la création d'un journal, nous avons choisi de faire un hebdomadaire sur l’actualité culturelle en France et dans le monde. <br> Je me suis inspiré de ce qui se faisait déjà dans les journaux concurrents pour réaliser la couverture. <br> Notre campagne de pub pour promouvoir la sortie de notre premier numéro était de coller la couverture en grand format dans les rues, j'ai donc ajouté cet effet de poster collé pour simuler le rendu final de notre campagne.",
-  },  
+    explications:
+      "Cette affiche a été réalisée dans le cadre d'un travail pour Mme Boissier (culture générale).<br> Le travail consistait en la création d'un journal, nous avons choisi de faire un hebdomadaire sur l’actualité culturelle en France et dans le monde. <br> Je me suis inspiré de ce qui se faisait déjà dans les journaux concurrents pour réaliser la couverture. <br> Notre campagne de pub pour promouvoir la sortie de notre premier numéro était de coller la couverture en grand format dans les rues, j'ai donc ajouté cet effet de poster collé pour simuler le rendu final de notre campagne.",
+  },
   {
     imgName: "Drogues.jpg",
     projectName: "Projet 6",
     subtitle: "Affiche de conférence",
     date: "03 2020",
-    explications: "Cette affiche a été réalisée dans le cadre d'un travail pour Mme Boissier (culture générale).<br>Le sujet choisi était l'utilisation des drogues à travers l'histoire.<br>À travers cette affiche, je voulais montrer qu'au-delà de l'aspect récréatif ou addictif des drogues qui est très souvent décrié, un autre visage se cachait, symbolisant la médecine, qui, depuis l'aube des temps utilise les drogues pour essayer de soigner des maux.",
+    explications:
+      "Cette affiche a été réalisée dans le cadre d'un travail pour Mme Boissier (culture générale).<br>Le sujet choisi était l'utilisation des drogues à travers l'histoire.<br>À travers cette affiche, je voulais montrer qu'au-delà de l'aspect récréatif ou addictif des drogues qui est très souvent décrié, un autre visage se cachait, symbolisant la médecine, qui, depuis l'aube des temps utilise les drogues pour essayer de soigner des maux.",
   },
   {
     imgName: "Logotypes.jpg",
     projectName: "Projet 7",
     subtitle: "Création de logotypes",
     date: "05 2020",
-    explications: "Lors de la création d'une marque de nombreux logos sont créés, certaines idées ne donnent aucune suite, mais d'autres sont reprises et développées. <br>Cette planche comporte les différentes propositions de logos pour la marque Simon Laks que nous devions développer pour un projet de Communication lors de ma première année. <br>Ainsi que plusieurs propositions pour mon propre logo. Les logos retenus sont les deux au centre.",
-  },  
+    explications:
+      "Lors de la création d'une marque de nombreux logos sont créés, certaines idées ne donnent aucune suite, mais d'autres sont reprises et développées. <br>Cette planche comporte les différentes propositions de logos pour la marque Simon Laks que nous devions développer pour un projet de Communication lors de ma première année. <br>Ainsi que plusieurs propositions pour mon propre logo. Les logos retenus sont les deux au centre.",
+  },
   {
     imgName: "Folio.jpg",
     projectName: "Projet 8",
-    subtitle: "Portfolio de mes autres créations",
+    subtitle: "Architecture 8",
     date: "04 2020",
-    explications: "Mon portfolio répertorie tous les projets de développement web que j'ai réalisés depuis que je suis à Hetic. <br>Cliquez sur mon nom à droite pour y accéder. -->",
-  }
-]
+    explications:
+      "Mon portfolio répertorie tous les projets de développement web que j'ai réalisés depuis que je suis à Hetic. <br>Cliquez sur mon nom à droite pour y accéder. -->",
+  },
+];
